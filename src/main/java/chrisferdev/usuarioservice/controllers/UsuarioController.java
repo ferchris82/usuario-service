5509bdc2fc5641a967e9066e11f721db7a940396
ChrisferDev <ferchris82@hotmail.com>
2024-05-17 17:01:33 -0500

    Versión 2.0

--- src/main/java/chrisferdev/usuarioservice/controllers/UsuarioController.java
+++ src/main/java/chrisferdev/usuarioservice/controllers/UsuarioController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import chrisferdev.usuarioservice.entities.Usuario;
 import chrisferdev.usuarioservice.service.UsuarioService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -37,9 +38,14 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarios);
     }
 
+    int cantidadReintentos = 1;
     @GetMapping("/{usuarioId}")
-    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    //@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<Usuario> obtenerUsuario(@PathVariable String usuarioId) {
+        log.info("Listar un solo usuario : UsuarioController");
+        log.info("Cantidad reintentos : {}", cantidadReintentos);
+        cantidadReintentos ++;
         Usuario usuario = usuarioService.getUsuario(usuarioId);
         return ResponseEntity.ok(usuario);
     }
